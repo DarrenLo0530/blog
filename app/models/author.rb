6d32cf7b357355933fc459f9474d69d140dbe241
Darren <darren.lo.0530@gmail.com>
2020-07-28 03:51:44 -0400

    Final iteration

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,8 @@
 class Author < ApplicationRecord
   authenticates_with_sorcery!
   validates_confirmation_of :password, message: "Should match confirmation", if: :password
-  has_many :articles
-  has_many :comments
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :username, :email, :crypted_password, presence: true
 end
